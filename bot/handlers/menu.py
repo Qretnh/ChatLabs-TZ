707e008dd47005c4bb84a56299ac3891583fd48e
Dubinin Andrey 3382 <144445325+Qretnh@users.noreply.github.com>
2025-03-27 23:02:21 +0300

    Update menu.py

--- bot/handlers/menu.py
+++ bot/handlers/menu.py
@@ -4,7 +4,7 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram_dialog import DialogManager
 
-from FSM import TASKS
+from FSM import FSMTodoList
 
 from services.api.users import register_user
 
@@ -13,4 +13,4 @@ router = Router()
 @router.message(Command(commands=['start']))
 async def menu(message: Message, dialog_manager: DialogManager):
     await register_user(str(message.from_user.id), message.from_user.username)
-    await dialog_manager.start(TASKS.start, data={"user_id": message.from_user.id})
+    await dialog_manager.start(FSMTodoList.start, data={"user_id": message.from_user.id})
