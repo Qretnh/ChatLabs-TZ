1b6df8f50e7670e25058f0bac3841f9eea55ec93
Dubinin Andrey 3382 <144445325+Qretnh@users.noreply.github.com>
2025-03-27 22:30:52 +0300

    Update users.py

--- bot/services/api/users.py
+++ bot/services/api/users.py
@@ -14,7 +14,5 @@ async def register_user(telegram_id: str, username: str):
     }
     async with httpx.AsyncClient() as client:
         response = await client.post(f"{BASE_URL}users/", json=payload)
-        if response.status_code != 201:
-            print(f"Ошибка регистрации юзера: {response.text}")
         response.raise_for_status()
         return response.json()
